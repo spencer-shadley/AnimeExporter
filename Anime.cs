6899bfc04653e9ddf79aae373f0a623de132c937
Spencer Shadley <spencershadley@utexas.edu>
2017-03-04 22:47:51 -0800

    added skeleton anime class

---
+++ Anime.cs
@@ -0,0 +1,30 @@
+﻿namespace AnimeExporter {
+    public class Anime {
+        // title
+        // MyAnimeList link
+        // rating
+        // categories
+        // rank
+        // popularity
+        // members
+        // synopsis
+        // background
+        // adaptation
+        // summary
+        // sequel
+        // related
+        // rating
+        // type
+        // number of episodes
+        // status
+        // aired
+        // permiered
+        // broadcast
+        // producers
+        // licensors
+        // studios
+        // source
+        // duration
+        // link to image
+    }
+}
