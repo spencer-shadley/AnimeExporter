a55cee1d71b840812e17617ce5d1cac66ae338ad
Spencer Shadley <spencershadley@utexas.edu>
2017-03-04 21:58:04 -0800

    initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace AnimeExporter
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("anime!");
+
+            // get anime as HTML from URL
+
+            // parse HTML
+
+            // make lots of requests to get detailed anime info (".hoverinfo_trigger")
+
+            // parse this to some nice code
+
+            // publish to a Google Sheet
+
+            // connect Tableau to that Sheet
+
+            // Make some cool vizzes
+
+            // Publish to Tableau Online
+        }
+    }
+}
