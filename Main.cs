31c8cc31b9bfbbe936024623ae504410b29c1e08
Spencer Shadley <spencershadley@utexas.edu>
2017-05-28 19:41:33 -0700

    wrote to my own google sheet successfully

--- Main.cs
+++ Main.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Net.NetworkInformation;
+using System.Security.Cryptography;
 using System.Threading;
 using AnimeExporter.Models;
 using Google.Apis.Auth.OAuth2;
@@ -12,10 +13,9 @@ using Google.Apis.Util.Store;
 
 namespace AnimeExporter {
     internal class Program {
-        
-        static string[] Scopes = {SheetsService.Scope.SpreadsheetsReadonly};
-        static string ApplicationName = "Google Sheets API";
-        
+        private static readonly string[] Scopes = {SheetsService.Scope.Spreadsheets};
+        private const string ApplicationName = "Google Sheets API";
+
         public static void Main(string[] args) {
 //            List<Anime> topAnime = HtmlParser.GetTopAnime(1);
 //            Console.WriteLine(string.Join(Environment.NewLine, topAnime));
@@ -24,14 +24,40 @@ namespace AnimeExporter {
         }
 
         public static void GoogleSheetsRunner() {
-            UserCredential credential;
-            
-            using (var stream =
-                new FileStream("client_secret.json", FileMode.Open, FileAccess.Read))
+            // Create Google Sheets API service.
+            var service = new SheetsService(new BaseClientService.Initializer()
             {
+                HttpClientInitializer = SetupCredentials(),
+                ApplicationName = ApplicationName,
+            });
+            
+            const string sheetId = "17KQKFy9o1pPG0Yko2dTYZcRhNSTdNWyI3NLWsJyfqbI";
+            const string updateRange = "A1";
+
+            var updateValues = new List<IList<object>>();
+
+            var someRow = new List<object> {"pizza?"};
+            updateValues.Add(someRow);
+            
+            ValueRange valueRange = new ValueRange {
+                Range = updateRange,
+                Values = updateValues
+            };
+
+            service.Spreadsheets.Values.Update(valueRange, sheetId, updateRange);
+            SpreadsheetsResource.ValuesResource.UpdateRequest updateRequest =
+                service.Spreadsheets.Values.Update(valueRange, sheetId, updateRange);
+            updateRequest.ValueInputOption = SpreadsheetsResource.ValuesResource.UpdateRequest.ValueInputOptionEnum.RAW;
+            updateRequest.Execute();
+        }
+
+        private static UserCredential SetupCredentials() {
+            UserCredential credential;
+            using (var stream =
+                new FileStream("client_secret.json", FileMode.Open, FileAccess.Read)) {
                 string credPath = Environment.GetFolderPath(
                     Environment.SpecialFolder.Personal);
-                credPath = Path.Combine(credPath, ".credentials/sheets.googleapis.com-dotnet-quickstart.json");
+                credPath = Path.Combine(credPath, ".credentials/sheets.googleapis.com-anime-exporter.json");
 
                 credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
                     GoogleClientSecrets.Load(stream).Secrets,
@@ -41,38 +67,7 @@ namespace AnimeExporter {
                     new FileDataStore(credPath, true)).Result;
                 Console.WriteLine("Credential file saved to: " + credPath);
             }
-
-            // Create Google Sheets API service.
-            var service = new SheetsService(new BaseClientService.Initializer()
-            {
-                HttpClientInitializer = credential,
-                ApplicationName = ApplicationName,
-            });
-            
-            // Define request parameters.
-            const string spreadsheetId = "1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms";
-            const string range = "Class Data!A2:E";
-            SpreadsheetsResource.ValuesResource.GetRequest request =
-                service.Spreadsheets.Values.Get(spreadsheetId, range);
-
-            // Prints the names and majors of students in a sample spreadsheet:
-            // https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
-            ValueRange response = request.Execute();
-            IList<IList<object>> values = response.Values;
-            if (values != null && values.Count > 0)
-            {
-                Console.WriteLine("Name, Major");
-                foreach (IList<object> row in values)
-                {
-                    // Print columns A and E, which correspond to indices 0 and 4.
-                    Console.WriteLine("{0}, {1}", row[0], row[4]);
-                }
-            }
-            else
-            {
-                Console.WriteLine("No data found.");
-            }
-            Console.Read();
+            return credential;
         }
     }
 }
