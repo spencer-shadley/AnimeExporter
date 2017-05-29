318137665e92b9515c17a228dad4c73bd221dbd2
Spencer Shadley <spencershadley@utexas.edu>
2017-05-28 19:13:03 -0700

    got the basic starter spreadsheet guide working

--- Main.cs
+++ Main.cs
@@ -1,12 +1,78 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Net.NetworkInformation;
+using System.Threading;
 using AnimeExporter.Models;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Sheets.v4.Data;
+using Google.Apis.Util.Store;
 
 namespace AnimeExporter {
     internal class Program {
+        
+        static string[] Scopes = {SheetsService.Scope.SpreadsheetsReadonly};
+        static string ApplicationName = "Google Sheets API";
+        
         public static void Main(string[] args) {
-            List<Anime> topAnime = HtmlParser.GetTopAnime(1);
-            Console.WriteLine(string.Join(Environment.NewLine, topAnime));
+//            List<Anime> topAnime = HtmlParser.GetTopAnime(1);
+//            Console.WriteLine(string.Join(Environment.NewLine, topAnime));
+
+            GoogleSheetsRunner();
+        }
+
+        public static void GoogleSheetsRunner() {
+            UserCredential credential;
+            
+            using (var stream =
+                new FileStream("client_secret.json", FileMode.Open, FileAccess.Read))
+            {
+                string credPath = Environment.GetFolderPath(
+                    Environment.SpecialFolder.Personal);
+                credPath = Path.Combine(credPath, ".credentials/sheets.googleapis.com-dotnet-quickstart.json");
+
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+
+            // Create Google Sheets API service.
+            var service = new SheetsService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+            
+            // Define request parameters.
+            const string spreadsheetId = "1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms";
+            const string range = "Class Data!A2:E";
+            SpreadsheetsResource.ValuesResource.GetRequest request =
+                service.Spreadsheets.Values.Get(spreadsheetId, range);
+
+            // Prints the names and majors of students in a sample spreadsheet:
+            // https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+            ValueRange response = request.Execute();
+            IList<IList<object>> values = response.Values;
+            if (values != null && values.Count > 0)
+            {
+                Console.WriteLine("Name, Major");
+                foreach (IList<object> row in values)
+                {
+                    // Print columns A and E, which correspond to indices 0 and 4.
+                    Console.WriteLine("{0}, {1}", row[0], row[4]);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No data found.");
+            }
+            Console.Read();
         }
     }
 }
