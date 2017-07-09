6dd5f99480f8f105dea5405966733afa80031c26
Spencer Shadley <spencershadley@utexas.edu>
2017-07-09 16:34:04 -0700

    optimized trends for mobile on explore

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
     internal class Program {
 
         public static void Main(string[] args) {
-            Animes topAnimes = TopAnimePage.ScrapeTopAnimes(0, 100);
+            Animes topAnimes = TopAnimePage.ScrapeTopAnimes(0, 200);
             GoogleSheet.BackupData();
             GoogleSheet.PublishDataToGoogleSheet(topAnimes);
             GoogleSheet.PublishGenresToGoogleSheet(topAnimes);
