a4fa532a4a47cd43a87903e9a15863b3acc709eb
Spencer Shadley <spencershadley@utexas.edu>
2017-08-13 16:03:24 -0700

    added basic test example

---
+++ test/AnimesTest.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+
+namespace AnimeExporter.test {
+    
+    [TestFixture]
+    public class AnimesTest {
+        private bool? _yes;
+        private bool? _no;
+        
+        [SetUp]
+        public void Init() {
+            Assert.IsNull(this._yes);
+            Assert.IsNull(this._no);
+
+            this._yes = true;
+            this._no = false;
+        }
+
+        [TearDown]
+        public void Cleanup() {
+            Assert.IsNotNull(this._yes);
+            Assert.IsNotNull(this._no);
+            
+            this._yes = null;
+            this._no = null;
+        }
+        
+        [Test]
+        public void PassTest() {
+            Assert.IsTrue(this._yes);
+            Assert.IsFalse(this._no);
+        }
+
+        [Test]
+        public void FailTest() {
+            Assert.IsNull(this._yes);
+            Assert.IsNull(this._no);
+        }
+    }
+}
