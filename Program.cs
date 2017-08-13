ef3a1b0869f77d39f9ca2e7a19b2c377d3136d74
Spencer Shadley <spencershadley@utexas.edu>
2017-08-13 15:37:06 -0700

    adjusted max number of pages

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
     internal class Program {
 
         public static void Main(string[] args) {
-            Animes topAnimes = TopAnimePage.ScrapeTopAnimes(0, 200);
+            Animes topAnimes = TopAnimePage.ScrapeTopAnimes(0, 150);
             GoogleSheet.BackupData();
             GoogleSheet.PublishDataToGoogleSheet(topAnimes);
             GoogleSheet.PublishGenresToGoogleSheet(topAnimes);
