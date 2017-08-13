a273210cee5c29de6d8c9eec87753eab4c5291ff
Spencer Shadley <spencershadley@utexas.edu>
2017-08-13 16:53:19 -0700

    refactored to use nested class fixtures

--- AnimeExporterTests/test/AnimesTest.cs
+++ AnimeExporterTests/test/AnimesTest.cs
@@ -4,40 +4,112 @@ using System.Linq;
 using AnimeExporter;
 using NUnit.Framework;
 
+using Attribute = AnimeExporter.Attribute;
+
 namespace AnimeExporterTests.test {
     
-    [TestFixture]
     public class AnimesTest {
-        private Animes _animes;
+        private const string Title = "Kimi no na wa."; 
         
-        [SetUp]
-        public void Init() {
-            this._animes = new Animes();
+        public Animes Animes;
+
+        public AnimesTest() {
+            this.Animes = new Animes();
         }
 
-        [TearDown]
-        public void Cleanup() {
-            this._animes = null;
-        }
-
+        // TODO: this should be a fixture
         [Test]
         public void TestConstruction() {
             // TODO
         }
 
-        [Test]
-        public void TestAdd() {
-            const string title = "Kimi no na wa.";
-            
-            var animeToAdd = new Anime {Title = new Attribute(title)};
-            this._animes.Add(animeToAdd);
+        [TestFixture]
+        public class AddAnime {
 
-            List<Anime> animes = this._animes.ToList();
+            private AnimesTest _test;
+
+            [SetUp]
+            public void Setup() {
+                this._test = new AnimesTest();
+            }
+
+            [TearDown]
+            public void Teardown() {
+                this._test = null;
+            }
+
+            [Test]
+            public void AddNull() {
+                
+            }
+            
+            [Test]
+            public void AddEmpty() {
+                
+            }
+
+            [Test]
+            public void AddSingleAnime() {
+                Anime animeToAdd = CreateAnime();
+                this._test.Animes.Add(animeToAdd);
+
+                List<Anime> animes = this._test.Animes.ToList();
+            
+                Assert.That(animes.ToList(), Has.Count.EqualTo(1));
+            
+                Anime addedAnime = animes[0];
+                Assert.AreEqual(addedAnime, animeToAdd);
+            }
+
+            [Test]
+            public void AddManyAnime() {
+                
+            }
+
+            [Test]
+            public void AddAnimes() {
+                
+            }
+            
+        }
+
+        [Test]
+        public void TestAddAnime() {
+            
+            // empty anime
+            
+            // anime with stuff
+            
+            // null
+
+            Anime animeToAdd = CreateAnime();
+            this.Animes.Add(animeToAdd);
+
+            List<Anime> animes = this.Animes.ToList();
             
             Assert.That(animes.ToList(), Has.Count.EqualTo(1));
             
             Anime addedAnime = animes[0];
             Assert.AreEqual(addedAnime, animeToAdd);
+        }
+        
+        [Test]
+        public void TestAddAnimes() {
+            const string title = "Kimi no na wa.";
+            
+            var animeToAdd = new Anime {Title = new Attribute(title)};
+            this.Animes.Add(animeToAdd);
+
+            List<Anime> animes = this.Animes.ToList();
+            
+            Assert.That(animes.ToList(), Has.Count.EqualTo(1));
+            
+            Anime addedAnime = animes[0];
+            Assert.AreEqual(addedAnime, animeToAdd);
+        }
+
+        private static Anime CreateAnime() {
+            return new Anime {Title = new Attribute(Title)};
         }
     }
 }
