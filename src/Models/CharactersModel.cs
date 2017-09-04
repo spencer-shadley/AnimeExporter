e5a144635961555e89bcdd8d1b73e54543fe81e1
Spencer Shadley <spencershadley@utexas.edu>
2017-09-03 21:18:41 -0700

    fleshed out model info to scrape

--- src/Models/CharactersModel.cs
+++ src/Models/CharactersModel.cs
@@ -1,9 +1,28 @@
 ﻿namespace AnimeExporter.Models {
     public class CharactersModel : DataModel {
-        // TODO: Add  Characters & Staff (https://myanimelist.net/anime/1794/Dirty_Pair_no_Ooshoubu__Nolandia_no_Nazo/characters)
-        // Characters
-        // Voice actors
-        // Staff
-        // Dub Info (check if the voice actors have "English")
+
+        // TODO: Add  Characters & Staff (https://myanimelist.net/anime/5114/Fullmetal_Alchemist__Brotherhood/characters)
+       
+        public AttributeModel IsInJapanese  = "Dubbed in Japanese";
+        public AttributeModel IsInEnglish   = "Dubbed in English";
+        public AttributeModel IsInSpanish   = "Dubbed in Spanish";
+        public AttributeModel IsInFrench    = "Dubbed in French";
+        public AttributeModel IsInKorean    = "Dubbed in Korean";
+        public AttributeModel IsInItalian   = "Dubbed in Italian";
+        public AttributeModel IsInBrazilian = "Dubbed in Brazilian";
+        public AttributeModel IsInGerman    = "Dubbed in German";
+        public AttributeModel IsInHungarian = "Dubbed in Hungarian";
+
+        public AttributeModel Producer               = "Producer";
+        public AttributeModel Director               = "Director";
+        public AttributeModel SoundDirector          = "Sound Director";
+        public AttributeModel EpisodeDirector        = "Episode Director";
+        public AttributeModel CharacterDesign        = "Character Designer";
+        public AttributeModel Storyboarder           = "Storyboarder";
+        public AttributeModel ExecutiveProducer      = "Executive Producer";
+        public AttributeModel ChiefAnimationDirector = "Chief Animation Director";
+        public AttributeModel DirectorOfPhotography  = "Director of Photography";
+        public AttributeModel AnimationDirector      = "Animation Director";
+        public AttributeModel CharacterDesigner      = "Character Designer";
     }
 }
