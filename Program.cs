5747a941b53c7efdbdcb519857200e703c5fe606
Spencer Shadley <spencershadley@utexas.edu>
2017-09-05 11:25:17 -0700

    Updated Google API request payload exceeded detection

--- Program.cs
+++ Program.cs
@@ -1,12 +1,11 @@
-﻿using AnimeExporter.Controllers;
-using AnimeExporter.Models;
+﻿using AnimeExporter.Models;
 using AnimeExporter.Views;
 
 namespace AnimeExporter {
     internal class Program {
 
         public static void Main(string[] args) {
-            AnimesModel animes = AnimesView.ScrapeTopAnimes(0, 125);
+            AnimesModel animes = AnimesView.ScrapeTopAnimes(0, 100);
             GoogleSheetView.BackupData();
             GoogleSheetView.PublishDataToGoogleSheet(animes);
             GoogleSheetView.PublishGenresToGoogleSheet(animes);
